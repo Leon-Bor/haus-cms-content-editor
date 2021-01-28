1c1d782e4814d4b1c08c184b363bb82a8332e9da
Leon <leon@piloteers.tech>
2021-01-28 21:39:08 +0100

    fix enter bug

--- src/app.tsx
+++ src/app.tsx
@@ -15,6 +15,7 @@ export const App = () => {
   const [isAuth, setIsAuth] = useState(
     (localStorage.getItem('hausAdminToken') && settings?.adminToken === localStorage.getItem('hausAdminToken')) || false
   );
+  const [hideContentEditor, setHideContentEditor] = useState(false);
   const [isEditiing, setIsEditiing] = useState(false);
   const [isSelectingLang, setIsSelectingLang] = useState(false);
   const [editedContent, setEditedContent] = useState({});
@@ -27,6 +28,10 @@ export const App = () => {
     const edit = urlParams.get('edit');
     if (edit) {
       setCookie('hausEdit', edit + '', 365);
+
+      if (edit === 'false') {
+        setHideContentEditor(true);
+      }
     }
 
     //testing stugg
@@ -44,7 +49,13 @@ export const App = () => {
       $('[data-haus-img]').attr('imageEditable', 'true');
       $('[data-haus-id]').on('input', function (this: any): void {
         console.log('Content changed: ' + $(this).attr('data-haus-id'));
-        setEditedContent({ ...this.editedContent, [$(this).attr('data-haus-id') as string]: $(this).html() });
+        setEditedContent({
+          ...this.editedContent,
+          [$(this).attr('data-haus-id') as string]: $(this)
+            .html()
+            .replace(/<div>/gi, '<br>')
+            .replace(/<\/div>/gi, ''),
+        });
       });
       $('[data-haus-id]').on('focus', function (this: any): void {
         $(this).addClass('haus-element--focused');
@@ -122,63 +133,64 @@ export const App = () => {
   };
 
   return (
-    <div className='haus-editor'>
-      {isAuth ? (
-        <>
-          <div className='haus-actions'>
-            <div
-              style={{ left: '0px' }}
-              className={`haus-icon haus-edit-icon ${isEditiing ? 'haus-icon--hide' : ''}`}
-              onClick={() => setEditing(true)}
-            >
-              <svg x='0px' y='0px' viewBox='0 0 528.899 528.899'>
-                <path
-                  d='M328.883,89.125l107.59,107.589l-272.34,272.34L56.604,361.465L328.883,89.125z M518.113,63.177l-47.981-47.981
-          c-18.543-18.543-48.653-18.543-67.259,0l-45.961,45.961l107.59,107.59l53.611-53.611
-          C532.495,100.753,532.495,77.559,518.113,63.177z M0.3,512.69c-1.958,8.812,5.998,16.708,14.811,14.565l119.891-29.069
-          L27.473,390.597L0.3,512.69z'
-                />
-              </svg>
-            </div>
-            <div
-              style={{ left: '0px' }}
-              className={`haus-icon haus-cancel-icon ${!isEditiing ? 'haus-icon--hide' : ''}`}
-              onClick={() => {
-                setEditing(false, false);
-                setIsSelectingLang(false);
-              }}
-            >
-              <svg x='0px' y='0px' viewBox='0 0 24 24'>
-                <path d='m14.828 12 5.303-5.303c.586-.586.586-1.536 0-2.121l-.707-.707c-.586-.586-1.536-.586-2.121 0l-5.303 5.303-5.303-5.304c-.586-.586-1.536-.586-2.121 0l-.708.707c-.586.586-.586 1.536 0 2.121l5.304 5.304-5.303 5.303c-.586.586-.586 1.536 0 2.121l.707.707c.586.586 1.536.586 2.121 0l5.303-5.303 5.303 5.303c.586.586 1.536.586 2.121 0l.707-.707c.586-.586.586-1.536 0-2.121z' />
-              </svg>
-            </div>
-            <div
-              style={{ left: isEditiing ? '65px' : '0px' }}
-              className={`haus-icon haus-accept-icon ${!isEditiing ? 'haus-icon--hide' : ''}`}
-              onClick={() => {
-                setEditing(false, true);
-                setIsSelectingLang(false);
-              }}
-            >
-              <svg x='0px' y='0px' viewBox='0 0 24 24'>
-                <g>
-                  <path d='m9.707 19.121c-.187.188-.442.293-.707.293s-.52-.105-.707-.293l-5.646-5.647c-.586-.586-.586-1.536 0-2.121l.707-.707c.586-.586 1.535-.586 2.121 0l3.525 3.525 9.525-9.525c.586-.586 1.536-.586 2.121 0l.707.707c.586.586.586 1.536 0 2.121z' />
-                </g>
-              </svg>
-            </div>
-          </div>
-          {settings?.enableI18n ? (
+    hideContentEditor == false && (
+      <div className='haus-editor'>
+        {isAuth ? (
+          <>
             <div className='haus-actions'>
               <div
                 style={{ left: '0px' }}
-                className={`haus-icon haus-language-icon ${isEditiing ? 'haus-icon--hide' : ''}`}
+                className={`haus-icon haus-edit-icon ${isEditiing ? 'haus-icon--hide' : ''}`}
+                onClick={() => setEditing(true)}
+              >
+                <svg x='0px' y='0px' viewBox='0 0 528.899 528.899'>
+                  <path
+                    d='M328.883,89.125l107.59,107.589l-272.34,272.34L56.604,361.465L328.883,89.125z M518.113,63.177l-47.981-47.981
+          c-18.543-18.543-48.653-18.543-67.259,0l-45.961,45.961l107.59,107.59l53.611-53.611
+          C532.495,100.753,532.495,77.559,518.113,63.177z M0.3,512.69c-1.958,8.812,5.998,16.708,14.811,14.565l119.891-29.069
+          L27.473,390.597L0.3,512.69z'
+                  />
+                </svg>
+              </div>
+              <div
+                style={{ left: '0px' }}
+                className={`haus-icon haus-cancel-icon ${!isEditiing ? 'haus-icon--hide' : ''}`}
                 onClick={() => {
-                  setIsSelectingLang(!isSelectingLang);
+                  setEditing(false, false);
+                  setIsSelectingLang(false);
                 }}
               >
-                <svg viewBox='0 0 512 512'>
-                  <path
-                    d='M437.02,74.98C388.667,26.629,324.38,0,256,0S123.333,26.629,74.98,74.98C26.629,123.332,0,187.62,0,256
+                <svg x='0px' y='0px' viewBox='0 0 24 24'>
+                  <path d='m14.828 12 5.303-5.303c.586-.586.586-1.536 0-2.121l-.707-.707c-.586-.586-1.536-.586-2.121 0l-5.303 5.303-5.303-5.304c-.586-.586-1.536-.586-2.121 0l-.708.707c-.586.586-.586 1.536 0 2.121l5.304 5.304-5.303 5.303c-.586.586-.586 1.536 0 2.121l.707.707c.586.586 1.536.586 2.121 0l5.303-5.303 5.303 5.303c.586.586 1.536.586 2.121 0l.707-.707c.586-.586.586-1.536 0-2.121z' />
+                </svg>
+              </div>
+              <div
+                style={{ left: isEditiing ? '65px' : '0px' }}
+                className={`haus-icon haus-accept-icon ${!isEditiing ? 'haus-icon--hide' : ''}`}
+                onClick={() => {
+                  setEditing(false, true);
+                  setIsSelectingLang(false);
+                }}
+              >
+                <svg x='0px' y='0px' viewBox='0 0 24 24'>
+                  <g>
+                    <path d='m9.707 19.121c-.187.188-.442.293-.707.293s-.52-.105-.707-.293l-5.646-5.647c-.586-.586-.586-1.536 0-2.121l.707-.707c.586-.586 1.535-.586 2.121 0l3.525 3.525 9.525-9.525c.586-.586 1.536-.586 2.121 0l.707.707c.586.586.586 1.536 0 2.121z' />
+                  </g>
+                </svg>
+              </div>
+            </div>
+            {settings?.enableI18n ? (
+              <div className='haus-actions'>
+                <div
+                  style={{ left: '0px' }}
+                  className={`haus-icon haus-language-icon ${isEditiing ? 'haus-icon--hide' : ''}`}
+                  onClick={() => {
+                    setIsSelectingLang(!isSelectingLang);
+                  }}
+                >
+                  <svg viewBox='0 0 512 512'>
+                    <path
+                      d='M437.02,74.98C388.667,26.629,324.38,0,256,0S123.333,26.629,74.98,74.98C26.629,123.332,0,187.62,0,256
          s26.629,132.668,74.98,181.02C123.333,485.371,187.62,512,256,512s132.667-26.629,181.02-74.98
          C485.371,388.668,512,324.38,512,256S485.371,123.332,437.02,74.98z M207.449,35.268c-8.587,8.353-17.868,18.307-27.154,29.893
          c-16.058,20.037-29.429,41.603-39.887,64.282h-71.56C100.811,82.328,150.083,47.876,207.449,35.268z M51.695,159.443h76.819
@@ -198,43 +210,44 @@ export const App = () => {
          c8.587-8.353,17.868-18.307,27.154-29.893c16.058-20.037,29.429-41.603,39.887-64.282h71.56
          C411.19,429.672,361.918,464.124,304.552,476.732z M460.305,352.557h-76.819c8.743-26.116,13.829-53.433,15.157-81.557h82.847
          C479.58,300.009,472.177,327.538,460.305,352.557z'
-                  />
-                </svg>
-                {localStorage.getItem('haus-i18nLanguage') && (
-                  <div className='haus-current-lang'>
-                    <FlagSvg i18n={localStorage.getItem('haus-i18nLanguage')} />
-                  </div>
-                )}
+                    />
+                  </svg>
+                  {localStorage.getItem('haus-i18nLanguage') && (
+                    <div className='haus-current-lang'>
+                      <FlagSvg i18n={localStorage.getItem('haus-i18nLanguage')} />
+                    </div>
+                  )}
+                </div>
+                {settings?.i18nLanguages?.map((l, i) => {
+                  return (
+                    <div
+                      key={i}
+                      style={{ left: isSelectingLang ? 65 * (i + 1) + 'px' : '0px', zIndex: 1000000025 - i }}
+                      className={`haus-lang ${!isSelectingLang || isEditiing ? 'haus-lang--hide' : ''}`}
+                      onClick={() => {
+                        setIsSelectingLang(false);
+                        var url = new URL(window.location.href);
+                        url.searchParams.set('language', l);
+                        window.location.href = url.toString();
+                        localStorage.setItem('haus-i18nLanguage', l);
+                      }}
+                    >
+                      <FlagSvg i18n={l} />
+                    </div>
+                  );
+                })}
               </div>
-              {settings?.i18nLanguages?.map((l, i) => {
-                return (
-                  <div
-                    key={i}
-                    style={{ left: isSelectingLang ? 65 * (i + 1) + 'px' : '0px', zIndex: 1000000025 - i }}
-                    className={`haus-lang ${!isSelectingLang || isEditiing ? 'haus-lang--hide' : ''}`}
-                    onClick={() => {
-                      setIsSelectingLang(false);
-                      var url = new URL(window.location.href);
-                      url.searchParams.set('language', l);
-                      window.location.href = url.toString();
-                      localStorage.setItem('haus-i18nLanguage', l);
-                    }}
-                  >
-                    <FlagSvg i18n={l} />
-                  </div>
-                );
-              })}
-            </div>
-          ) : null}
-        </>
-      ) : (
-        <LockAuth
-          isAuth={isAuth}
-          onAuthenticate={async () => {
-            return await authenticate();
-          }}
-        />
-      )}
-    </div>
+            ) : null}
+          </>
+        ) : (
+          <LockAuth
+            isAuth={isAuth}
+            onAuthenticate={async () => {
+              return await authenticate();
+            }}
+          />
+        )}
+      </div>
+    )
   );
 };
